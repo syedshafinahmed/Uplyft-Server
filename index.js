d3d96778db699d146dae0b8c96a91cf8a6fc33e7
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-11-12 22:31:15 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ const verifyToken = async (req, res, next) => {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("event-db");
     const eventCollection = db.collection("events");
@@ -183,7 +183,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
